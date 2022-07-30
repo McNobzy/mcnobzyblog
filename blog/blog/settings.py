24c69b5eb8d0d376d733452bc91c30da3aee5e70
Olaoluwa <olaoluwa.nwobodo@aun.edu.ng>
2022-07-30 13:43:13 +0100

    Third commit

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9@jfggxs%mh(!nt-21!swl6whb12nny5=@ma73m+y5+iawrm!'
+# SECRET_KEY = 'django-insecure-&9@jfggxs%mh(!nt-21!swl6whb12nny5=@ma73m+y5+iawrm!'
 
+SECRET_KEY = '85p_zzhk6hihl^skxa!92#n4=vj09bgom(*-i3do8pci1ny7!2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
